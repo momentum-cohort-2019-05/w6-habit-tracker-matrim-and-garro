595fff6ddeaef4cd76741dd5b0075b8e3080d025
Andy Dearing <andy.dearing@gmail.com>
2019-07-01 17:49:51 -0400

    working home page

--- habits/views.py
+++ habits/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "/habits/home.html", {
+    return render(request, "home.html", {
 
     })
