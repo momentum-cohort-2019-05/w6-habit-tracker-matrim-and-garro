1c7be37c4070ede58499db810d43f3d4ff50ad37
David Barnes <david.barnes115@gmail.com>
2019-07-01 17:13:02 -0400

    added model Habit and DailyRecord

--- habits/models.py
+++ habits/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from datetime import date
 
 class Habit(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(max_length=1000)
-    
+    # label = models.CharField(max_length=200)
+    # description = models.TextField(max_length=1000)
+    verb = models.CharField(max_length=200)
+    over = models.BooleanField(default=True)
+    quantity = models.PositiveIntegerField(default=0)
+    unit = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Record(models.Model):
-    pass
+
+class DailyRecord(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    quantity = models.PositiveIntegerField(default=0)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    
 
 class Comment(models.Model):
     pass
