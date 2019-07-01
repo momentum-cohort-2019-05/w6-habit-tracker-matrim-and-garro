2abe41da1efecdb1e7749f3bf1011645938303e1
Andy Dearing <andy.dearing@gmail.com>
2019-07-01 18:14:21 -0400

    working 3 page link

--- habits/urls.py
+++ habits/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('habit-manager/', views.habit_manager, name="habit-manager"),
+    path('social/', views.social, name='social')
 ]
--- habits/views.py
+++ habits/views.py
@@ -6,3 +6,13 @@ def home(request):
     return render(request, "home.html", {
 
     })
+
+def habit_manager(request):
+    return render(request, "habit_manager.html", {
+
+    })
+
+def social(request):
+    return render(request, "social.html", {
+
+    })
