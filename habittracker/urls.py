d557f409d32b51ede02b4f8c874b44bde91c3046
Andy Dearing <andy.dearing@gmail.com>
2019-07-02 09:07:31 -0400

    style and login links done for base.html

--- habittracker/urls.py
+++ habittracker/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib import admin
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
