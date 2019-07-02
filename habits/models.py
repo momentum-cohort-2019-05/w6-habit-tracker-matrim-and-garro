f658fa5928775617e347d7c1267e2bf546f2c58e
Andy Dearing <andy.dearing@gmail.com>
2019-07-02 16:00:32 -0400

    working latest dailyrecord

--- habits/models.py
+++ habits/models.py
@@ -12,12 +12,17 @@ class Habit(models.Model):
     unit = models.CharField(max_length=200)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
     def __str__(self):
         if self.over == True:
             over_str = "over"
         else:
             over_str = "under"
         return f'{self.verb} {over_str} {self.quantity} {self.unit} per day'
+    
+    def last_update(self):
+        """returns the DailyRecord object of the last update"""
+        return self.dailyrecord_set.order_by('-date').first()
 
 
 class DailyRecord(models.Model):
@@ -25,6 +30,8 @@ class DailyRecord(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"{self.date}|{self.quantity}|{self.habit}"
 
 class Comment(models.Model):
     pass
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from habits.models import Habit, DailyRecord
 from django.contrib.auth.decorators import login_required
+from datetime import date
 
 # Create your views here.
 
@@ -13,11 +14,18 @@ def home(request):
 @login_required
 def habit_manager(request, pk):
     list_of_habits = Habit.objects.filter(owner__pk=pk)
-
-
-
+    today = date.today()
+    updated_today = {}
+    for habit in list_of_habits:
+        if habit.last_update().date == today:
+            habit.updated_today = True
+        else:
+            habit.updated_today = False
+    
+    # breakpoint()
     return render(request, "habit_manager.html", {
-        "list_of_habits": list_of_habits
+        'list_of_habits' : list_of_habits,
+        'updated_today' : updated_today
     })
 
 def social(request):
