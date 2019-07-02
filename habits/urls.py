12b20c3b7b4825324d7a352b90aecc1d337ad08c
Andy Dearing <andy.dearing@gmail.com>
2019-07-02 18:36:44 -0400

    working form for create

--- habits/urls.py
+++ habits/urls.py
@@ -6,5 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('habit-manager/<int:pk>', views.habit_manager, name="habit-manager"),
-    path('social/', views.social, name='social')
+    path('social/', views.social, name='social'),
+    path('create-daily-record/<int:pk>', views.create_daily_record, name='create-daily-record'),
+    path('edit-daily-record/<int:pk>', views.create_daily_record, name='edit-daily-record'),
 ]
