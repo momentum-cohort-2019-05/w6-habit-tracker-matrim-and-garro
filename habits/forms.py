e5d49a14fd5d535e542a312aec98c1130d6c2ad4
Andy Dearing <andy.dearing@gmail.com>
2019-07-02 21:05:34 -0400

    functions for adding a habit

--- habits/forms.py
+++ habits/forms.py
@@ -6,3 +6,9 @@ class CreateDailyRecord(forms.Form):
 
 class EditDailyRecord(forms.Form):
     quantity = forms.IntegerField()
+
+class CreateHabit(forms.Form):
+    verb = forms.CharField(max_length=50)
+    over = forms.BooleanField()
+    quantity = forms.IntegerField()
+    unit = forms.CharField(max_length=50)
