1622edc710e32d2c88d2ff68c271da69f3dffca0
Andy Dearing <andy.dearing@gmail.com>
2019-07-03 13:04:05 -0400

    makes model Comment

--- habits/forms.py
+++ habits/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from habits.models import Habit, DailyRecord, Buddy
+from habits.models import Habit, DailyRecord
 
 class CreateDailyRecord(forms.Form):
     quantity = forms.IntegerField()
